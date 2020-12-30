eccd8788eda81e87c9a8323bd9f9cf68eef2cee4
Sven Meinhardt <sven.meinhardt@posteo.de>
2020-12-30 16:45:06 +0000

    adding a spinner while redirecting

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { withAuthenticationRequired } from '@auth0/auth0-react'
+import { withAuthenticationRequired, useAuth0 } from '@auth0/auth0-react'
+import styled from 'styled-components'
 import { Info, Repos, User, Search, Navbar } from '../components'
 import loadingImage from '../images/preloader.gif'
 
 import { GithubContext } from '../context/context'
 const Dashboard = () => {
   const { isLoading } = React.useContext(GithubContext)
+  const { error } = useAuth0()
+
+  if (error)
+    return (
+      <Wrapper>
+        <h1>{error.message}</h1>
+      </Wrapper>
+    )
 
   if (isLoading)
     return (
@@ -27,4 +36,19 @@ const Dashboard = () => {
   )
 }
 
-export default withAuthenticationRequired(Dashboard)
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  img {
+    width: 150px;
+  }
+`
+
+export default withAuthenticationRequired(Dashboard, {
+  onRedirecting: () => (
+    <Wrapper>
+      <img src={loadingImage} alt='spinner' />
+    </Wrapper>
+  ),
+})
